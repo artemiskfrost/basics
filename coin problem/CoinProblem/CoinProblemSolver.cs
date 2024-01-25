7dce923dbf4f0c6b0d415964fd21bb9b8fc57206
Artemis K. Frost (she/her) <github@artemiskfrost.com>
2024-01-25 15:51:36 -0700

    roughly implement coin problem solving

--- coin problem/CoinProblem/CoinProblemSolver.cs
+++ coin problem/CoinProblem/CoinProblemSolver.cs
@@ -1,8 +1,37 @@
-
-
-namespace ArtemisKFrost.Basics
+namespace CoinProblem
 {
 	public class CoinProblemSolver
 	{
+		private Dictionary< long, long > SubtotalSolutions = new();
+		private int[] CoinValues { get; set; } = Array.Empty<int>;
+
+
+		public long Solve( long total, int[] coinValues )
+		{
+			CoinValues = coinValues;
+			return SolveSubtotal( total );
+		}
+
+
+		private long SolveSubtotal( long subtotal )
+		{
+			if ( subtotal < 1 )
+			{
+				return 0;
+			}
+
+			long solutionCount = 0;
+			if ( SubtotalSolutions.TryGetValue( subtotal, out solutionCount ) )
+			{
+				return solutionCount;
+			}
+
+			foreach ( var coinValue in coinValues )
+			{
+				solutionCount += SolveSubtotal( total - coinValue );
+			}
+			SubtotalSolutions.Add( subtotal, solutionCount );
+			return solutionCount;
+		}
 	}
 }
